7b1c813305deb4c704b4da2a2d1273e2709c8352
viktorburcev <victor.burtsev@gmail.com>
2017-01-25 22:48:45 +0300

    Added @Data and @AllArgsConstructor in StackoverflowWebsite.java

--- src/main/java/de/springboot/model/StackoverflowWebsite.java
+++ src/main/java/de/springboot/model/StackoverflowWebsite.java
@@ -1,37 +1,15 @@
 package de.springboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class StackoverflowWebsite {
-    public final String id;
-    public final String website;
-    public final String iconImageUrl;
-    public final String title;
-    public final String description;
+    public String id;
+    public String website;
+    public String iconImageUrl;
+    public String title;
+    public String description;
 
-    public StackoverflowWebsite(String id, String website, String iconImageUrl, String title, String description) {
-        this.id = id;
-        this.website = website;
-        this.iconImageUrl = iconImageUrl;
-        this.title = title;
-        this.description = description;
-    }
-
-    public String getIconImageUrl() {
-        return iconImageUrl;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
