4386525911bbe1b633002683ea2c4f179b1f7fb1
viktorburcev <victor.burtsev@gmail.com>
2017-01-27 21:20:22 +0300

    Init

---
+++ src/main/resources/database/SpringDemo.js
@@ -0,0 +1,44 @@
+db.stackoverflowWebsite.insert({
+    _id: '1.1',
+    _class: 'de.springboot.model.StackoverflowWebsite',
+    website: '1.2',
+    iconImageUrl: 'http://www.iconsearch.ru/uploads/icons/web2/32x32/favorite_32x32.png',
+    title: '1.4',
+    description: '1.5'
+});
+
+db.stackoverflowWebsite.insert({
+    _id: '2.1',
+    _class: 'de.springboot.model.StackoverflowWebsite',
+    website: '2.2',
+    iconImageUrl: 'http://www.iconsearch.ru/uploads/icons/web2/32x32/favorite_32x32.png',
+    title: '2.4',
+    description: '2.5'
+});
+
+db.stackoverflowWebsite.insert({
+    _id: '3.1',
+    _class: 'de.springboot.model.StackoverflowWebsite',
+    website: '3.2',
+    iconImageUrl: 'http://www.iconsearch.ru/uploads/icons/web2/32x32/favorite_32x32.png',
+    title: '3.4',
+    description: '3.5'
+});
+
+db.stackoverflowWebsite.insert({
+    _id: '4.1',
+    _class: 'de.springboot.model.StackoverflowWebsite',
+    website: '4.2',
+    iconImageUrl: 'http://www.iconsearch.ru/uploads/icons/web2/32x32/favorite_32x32.png',
+    title: '4.4',
+    description: '4.5'
+});
+
+db.stackoverflowWebsite.insert({
+    _id: '5.1',
+    _class: 'de.springboot.model.StackoverflowWebsite',
+    website: '5.2',
+    iconImageUrl: 'http://www.iconsearch.ru/uploads/icons/web2/32x32/favorite_32x32.png',
+    title: '5.4',
+    description: '5.5'
+});
