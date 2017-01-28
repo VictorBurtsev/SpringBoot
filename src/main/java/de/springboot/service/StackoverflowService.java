6c1505b94e4dc91b9b09e9b00a0ba8a03607eae9
viktorburcev <victor.burtsev@gmail.com>
2017-01-28 15:06:36 +0300

    Removed "import java.util.ArrayList;"

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -5,7 +5,6 @@ import de.springboot.persistence.StackoverflowWebsiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
