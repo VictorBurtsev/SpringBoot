edfd036141c8fab3936f88c3b690fa0b33e7c8c8
viktorburcev <victor.burtsev@gmail.com>
2017-01-28 22:44:53 +0300

    Added module "SpringDemoAPI"

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,16 +1,17 @@
 ﻿var app = angular.module("springDemo", []);
 
 app.controller("AppCtrl", function ($scope, $http) {
-    /*$scope.websites = [{
-     iconImageUrl: 'http://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico',
-     id: '2',
-     website: '3',
-     title: '4',
-     description: '5'
-     }];*/
-
     $scope.websites = [];
     $http.get('http://localhost:8081/api/ntc-4').success(function (data) {
+        $scope.websites = data;
+    });
+});
+
+var app = angular.module("springDemoAPI", []);
+
+app.controller("AppCtrl", function ($scope, $http) {
+    $scope.websites = [];
+    $http.get('http://localhost:8081/api/stackoverflow').success(function (data) {
         $scope.websites = data;
     });
 });
