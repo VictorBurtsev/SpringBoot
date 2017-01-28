60e098a5ccc426e36a9b4b46d724eaafbbbce1b6
viktorburcev <victor.burtsev@gmail.com>
2017-01-28 22:48:13 +0300

    Init class "StackExchangeClient.java"

---
+++ src/main/java/de/springboot/service/StackExchangeClient.java
@@ -0,0 +1,32 @@
+package de.springboot.service;
+
+import de.springboot.model.SiteDto;
+import de.springboot.model.SitesDto;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Component
+public class StackExchangeClient {
+    HttpClient httpClient = HttpClientBuilder.create().build();
+    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    private RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+    public List<SiteDto> getSites() {
+        String url = "https://api.stackexchange.com/2.2/sites?page=1&pagesize=1000&filter=!8IeYYZbP7kF8WkCg(i*zE";
+        try {
+            SitesDto response = restTemplate.getForObject(new URI(url), SitesDto.class);
+            return response.getItems();
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
