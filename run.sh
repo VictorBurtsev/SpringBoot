21465028804b56699e6e79363b03fdebf930adeb
viktorburcev <victor.burtsev@gmail.com>
2017-01-28 22:45:54 +0300

    Added two new links

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 mvn clean install
 sleep 5 && open http://localhost:8081/stackoverflow.html &
 sleep 5 && open http://localhost:8081/api/ntc-4 &
+sleep 5 && open http://localhost:8081/api/stackoverflow &
+sleep 5 && open http://localhost:8081/stackoverflowAPI.html &
 java -jar target/springboot-1.0-SNAPSHOT.jar --port 8081
