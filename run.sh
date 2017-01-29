1db0d6e059fa4415a7f05585776139939e5f585a
viktorburcev <victor.burtsev@gmail.com>
2017-01-29 21:15:17 +0300

    run - removed 3 "sleep 5" and update "sleep 10"

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 mvn clean install
-sleep 5 && open http://localhost:8081/stackoverflow.html &
-sleep 5 && open http://localhost:8081/api/ntc-4 &
-sleep 5 && open http://localhost:8081/api/stackoverflow &
-sleep 5 && open http://localhost:8081/stackoverflowAPI.html &
+sleep 10 &&
+open http://localhost:8081/stackoverflow.html &&
+open http://localhost:8081/api/ntc-4 &&
+open http://localhost:8081/api/stackoverflow &&
+open http://localhost:8081/stackoverflowAPI.html &
 java -jar target/springboot-1.0-SNAPSHOT.jar --port 8081
