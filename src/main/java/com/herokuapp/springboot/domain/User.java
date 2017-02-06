c9c35fbe552c22e147a983d955c04bc9b9db1388
viktorburcev <victor.burtsev@gmail.com>
2017-02-06 21:39:59 +0300

    User.java - added properties

--- src/main/java/com/herokuapp/springboot/domain/User.java
+++ src/main/java/com/herokuapp/springboot/domain/User.java
@@ -4,8 +4,17 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class User implements UserDetails {
+    private List<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
