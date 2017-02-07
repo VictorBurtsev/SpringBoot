4480967547b14104b3e9cbbdeb7251cc557494f3
viktorburcev <victor.burtsev@gmail.com>
2017-02-07 21:53:58 +0300

    UserService.java - added User.builder in UserDetails method

--- src/main/java/com/herokuapp/springboot/services/UserService.java
+++ src/main/java/com/herokuapp/springboot/services/UserService.java
@@ -1,12 +1,25 @@
 package com.herokuapp.springboot.services;
 
+import com.google.common.collect.ImmutableList;
+import com.herokuapp.springboot.domain.Role;
+import com.herokuapp.springboot.domain.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return User.builder()
+                .username(username)
+                .password("password")
+                .authorities(ImmutableList.of(Role.USER))
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .build();
     }
 }
