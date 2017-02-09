7aa71a57af07c12bc9aa778c10dede5a2aecbbc0
viktorburcev <victor.burtsev@gmail.com>
2017-02-09 21:43:31 +0300

    UserDao.java - Added MongoTemple property

--- src/main/java/com/herokuapp/springboot/persistance/UserDao.java
+++ src/main/java/com/herokuapp/springboot/persistance/UserDao.java
@@ -1,4 +1,9 @@
 package com.herokuapp.springboot.persistance;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 public class UserDao {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 }
