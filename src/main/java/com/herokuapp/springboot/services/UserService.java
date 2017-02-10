82036e9460d3b336f26af12f70690ba48601d893
viktorburcev <victor.burtsev@gmail.com>
2017-02-10 19:14:33 +0300

    UserService.java - Added init methods

--- src/main/java/com/herokuapp/springboot/services/UserService.java
+++ src/main/java/com/herokuapp/springboot/services/UserService.java
@@ -4,27 +4,38 @@ import com.google.common.collect.ImmutableList;
 import com.herokuapp.springboot.domain.Role;
 import com.herokuapp.springboot.domain.User;
 import com.herokuapp.springboot.persistance.UserDao;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserDao userDao;
 
+    @PostConstruct
+    public void init() {
+        if (!userDao.findByUsername("user").isPresent()) {
+            userDao.save(User.builder()
+                    .username("user")
+                    .password("password")
+                    .authorities(ImmutableList.of(Role.USER))
+                    .accountNonExpired(true)
+                    .accountNonLocked(true)
+                    .credentialsNonExpired(true)
+                    .enabled(true)
+                    .build());
+        }
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.builder()
-                .username(username)
-                .password("password")
-                .authorities(ImmutableList.of(Role.USER))
-                .accountNonExpired(true)
-                .accountNonLocked(true)
-                .credentialsNonExpired(true)
-                .enabled(true)
-                .build();
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        return userDao.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("user " + username + " was not found!"));
     }
 }
