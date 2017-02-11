4245bd7ef0aaca28fb7b63789b16a5682f7e4767
viktorburcev <victor.burtsev@gmail.com>
2017-02-11 10:57:10 +0300

    Role.java - Removed comments

--- src/main/java/com/herokuapp/springboot/domain/Role.java
+++ src/main/java/com/herokuapp/springboot/domain/Role.java
@@ -2,9 +2,6 @@ package com.herokuapp.springboot.domain;
 
 import org.springframework.security.core.GrantedAuthority;
 
-/**
- * Created by viktorburcev on 06.02.17.
- */
 public enum Role implements GrantedAuthority {
     USER;
 
