cee50e5aa966a2eca247c4ed81640a8857a39529
viktorburcev <victor.burtsev@gmail.com>
2017-02-13 21:40:06 +0300

    Application.java - Added showLoginPage method

--- src/main/java/com/herokuapp/springboot/Application.java
+++ src/main/java/com/herokuapp/springboot/Application.java
@@ -3,7 +3,9 @@ package com.herokuapp.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @SpringBootApplication
@@ -15,5 +17,14 @@ public class Application {
     @RequestMapping("/hello")
     public String showHelloWorld() {
         return "hello-world";
+    }
+
+    @RequestMapping("/login")
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error,
+                                @RequestParam(value = "logout", required = false) String logout,
+                                Model model) {
+        model.addAttribute("error", error != null);
+        model.addAttribute("logout", logout != null);
+        return "login";
     }
 }
